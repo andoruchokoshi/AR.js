5a964477501672ca0e6fb97bcbe3a5e4ad99d19d
andoruchokoshi <andoruchokoshi@mail.ru>
2023-05-11 17:44:50 +0800

    v. 0.3.0 udpate preview block, add image

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+const previewImg = document.getElementById("preview-img");
+const previewTitle = document.getElementById("preview-title");
+const previewDescription = document.getElementById("preview-description");
+
+const previewImg2 = document.getElementById("preview-img2");
+const previewTitle2 = document.getElementById("preview-title2");
+const previewDescription2 = document.getElementById("preview-description2");
+
+const images = [];
+
+fetch("resources/img/")
+    .then(response => response.text())
+    .then(data => {
+        const parser = new DOMParser();
+        const html = parser.parseFromString(data, "text/html");
+        const links = html.querySelectorAll("a");
+        links.forEach(link => {
+            const href = link.getAttribute("href");
+            if (/\.(jpe?g|png|gif)$/i.test(href)) {
+                images.push(href);
+            }
+        });
+        if (images.length === 0) {
+            console.log("No images found.");
+        } else {
+            setRandomImage(previewImg, previewTitle, previewDescription);
+            setRandomImage(previewImg2, previewTitle2, previewDescription2);
+        }
+    })
+    .catch(error => console.error(error));
+
+function setRandomImage(imgElem, titleElem, descElem) {
+    if (images.length === 0) {
+        console.log("No images found.");
+        return;
+    }
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    images.splice(randomIndex, 1);
+    imgElem.src = randomImage;
+    const filename = randomImage.split("/").pop();
+    const lastDotIndex = filename.lastIndexOf(".");
+    const lastSlashIndex = filename.lastIndexOf("\\");
+    const title = lastSlashIndex === -1 ? filename.substring(0, lastDotIndex) : filename.substring(lastSlashIndex + 1, lastDotIndex).replace(/\([^()]*\)/, '').trim();
+    const description = /\(([^)]+)\)/.exec(filename)[1];
+    titleElem.textContent = title;
+    descElem.textContent = description;
+}
